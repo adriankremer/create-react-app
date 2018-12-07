4755d0f7b7128cde4e52b489e2755becfb237010
Adrian Kremer <a.kremer@reply.de>
2018-12-07 14:34:23 +0100

    Changed file extension for TypeScript index file not containing any jsx from `.tsx` to `.ts`, changed `include` tsconfig property to component index file to prevent emitting declarations for the demo content

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -219,7 +219,7 @@ function verifyTypeScriptSetup() {
 
   // tsconfig will have the merged "include" and "exclude" by this point
   if (parsedTsConfig.include == null) {
-    appTsConfig.include = ['src'];
+    appTsConfig.include = ['src/index.ts'];
     messages.push(
       `${chalk.cyan('include')} should be ${chalk.cyan.bold('src')}`
     );
